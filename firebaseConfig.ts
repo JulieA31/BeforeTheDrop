f46ae38aae851837074c44bd1041ee7223fadc8f
JulieA31 <julie.alatrach@gmail.com>
2025-12-31 12:04:50 +0100

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Remplacez ces valeurs par celles de votre console Firebase
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyDummyKeyForPreviewMode",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "before-the-drop.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "before-the-drop",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "before-the-drop.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "123456789",
-  appId: process.env.FIREBASE_APP_ID || "1:123456789:web:abcdef"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
